6decac1e64086460108c670fd12d18cff38671c6
Junaid Ahmad <m.junaidahmad@outlook.com>
2018-01-06 00:28:10 +0500

    adnan changes

--- app/Http/Controllers/AdnanController.php
+++ app/Http/Controllers/AdnanController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class AdnanController extends Controller {
     public function test(Request $request) {
         $name = 'Adnan';
+        knljlkkl
         return view('adnan', compact('name'));
     }
 }
